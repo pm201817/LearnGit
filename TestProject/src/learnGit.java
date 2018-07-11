f6279e43dddaccf1d195a48739a0cc470b54af81
Prasad Masurkar <pmasurka@LIN19000343.corp.capgemini.com>
2018-07-11 15:54:03 +0530

    Added new Print statement

--- TestProject/src/learnGit.java
+++ TestProject/src/learnGit.java
@@ -10,6 +10,8 @@ public class learnGit {
 		System.out.println("Lets learn Git + Eclipse practivally...!!!");
 		
 		System.out.println("Learning merge conflict");
+		
+		System.out.println("New Developer added to project");
 	}
 
 }
